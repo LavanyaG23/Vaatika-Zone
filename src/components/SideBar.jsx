2280bc845bc856f6fad9304fbcc318d974b9cab6
LavanyaGokhale <92771328+LavanyaG23@users.noreply.github.com>
2024-02-25 10:29:42 +0530

    final sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom'; // Import useHistory hook
 import { SideBarData } from './SideBarData';
+import { people02 } from '../assets';
 
 const SideBar = () => {
+  const history = useNavigate(); // Initialize useHistory hook
+
+  // Dummy user data (replace with actual user data)
+  const user = {
+    username: "Utkarsh",
+    //profileImage: "path_to_user_image.jpg" // URL of the user's profile image
+  };
+
+  const handleLogout = () => {
+    // Perform logout actions (e.g., clear user session, etc.)
+    // Navigate to the home page
+    history("/adminloginpage");
+  };
+
   return (
     <div className='Sidebar'>
       <ul className='SidebarList'>
+        {/* User info */}
+        <li className='user-info'>
+          <div className='user-image'><img src={people02} alt='User' /></div>
+          <div className='welcome-message'>Welcome back, {user.username}!</div>
+        </li>
+        {/* Sidebar links */}
         {SideBarData.map((val, key) => (
           <NavLink key={key} to={val.link} activeClassName='active' className='link'>
             <li className='row'>
@@ -14,6 +35,10 @@ const SideBar = () => {
             </li>
           </NavLink>
         ))}
+        {/* Logout button */}
+        <li className='logout-button'>
+          <button onClick={handleLogout}>Logout</button>
+        </li>
       </ul>
     </div>
   );
